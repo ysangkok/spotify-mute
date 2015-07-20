b4596e2fdce3598a2b6e382d34260e411956fc4d
Janus <ysangkok@gmail.com>
2015-07-20 15:39:37 +0200

    remote machine muting

--- spotifymute_helper.sh
+++ spotifymute_helper.sh
@@ -5,8 +5,6 @@
 
 # set commercial mute, so we do not neet to listen to them
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 get_pactl_nr(){
     LC_ALL=C pacmd list-sink-inputs | awk -v binary="spotify" '
             $1 == "index:" {idx = $2}
@@ -18,41 +16,75 @@ get_pactl_nr(){
 
 mute(){
     for PACTLNR in $(get_pactl_nr); do
-      pactl set-sink-input-mute "$PACTLNR" yes
+      pactl set-sink-input-mute "$PACTLNR" "$@"
     done
 }
 
-unmute(){
-    for PACTLNR in $(get_pactl_nr); do
-        pactl set-sink-input-mute "$PACTLNR" no
-    done
+netw_prepare(){
+    NETW=0
+    if [[ $PULSE_SERVER != "" ]]; then
+        NETW=1
+        #PIP=$(mktemp -u)
+        #mkfifo "$PIP"
+        PSWDF=$(mktemp)
+        cat > $PSWDF
+        #REMOTE=$(sshpass -f $PSWDF ssh "$PULSE_SERVER" "mktemp")
+        #rsync --rsh="sshpass -f $PSWDF ssh $PULSE_SERVER" "$0" $REMOTE
+        #cat $PIP | sshpass -f $PSWDF ssh "$PULSE_SERVER" "chmod +x $REMOTE; $REMOTE" &
+	#echo DONE CATTING SCRIPT
+    fi
 }
 
-ID=$(wmctrl -lx | awk -F' ' '$3 == "spotify.Spotify" {print $1}')
+mute_div(){
+    if [ $NETW -eq 1 ]; then
+        #echo "mute '$@'" > "$PIP"
+        if [[ "$@" == yes ]]; then
+            sshpass -f $PSWDF ssh "$PULSE_SERVER" "amixer -D pulse set Master mute"
+        else
+            #echo "amixer -D pulse set Master unmute" > $PIP
+            sshpass -f $PSWDF ssh "$PULSE_SERVER" "amixer -D pulse set Master unmute"
+        fi
+    else
+        mute "$@"
+    fi
+}
 
-if [[ $ID == "" ]]; then
-        unmute
-        exit 1;
+
+main(){
+    DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+    netw_prepare
+
+    ID=$(wmctrl -lx | awk -F' ' '$3 == "spotify.Spotify" {print $1}')
+
+    if [[ $ID == "" ]]; then
+            mute_div no
+            exit 1;
+    fi
+
+    trap "mute_div no && trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+    when-changed "${BASH_SOURCE[0]}" "$DIR/blacklist.txt" -c "kill $$" &
+
+    while read -r XPROPOUTPUT; do
+            XPROP_TRACKDATA="$(echo "$XPROPOUTPUT" | cut -d \" -f 2 )"
+
+            # show something
+            # echo "Checking against: $XPROP_TRACKDATA"
+
+            #amixer -D pulse set Master unmute
+            mute_div no
+            while read -r LINE; do
+                #echo Checking $LINE
+                if grep -Fq "$LINE" <(echo "$XPROP_TRACKDATA"); then
+                    #amixer -D pulse set Master mute
+                    mute_div yes
+                    break
+                fi
+            done < "$DIR/blacklist.txt"
+    done < <(xprop -spy -id "$ID" _NET_WM_NAME)
+}
+
+if [[ ${BASH_SOURCE[0]} = $0 ]]; then
+    main "$@"
 fi
-
-trap "echo Dying!; exit" TERM
-
-when-changed "${BASH_SOURCE[0]}" "$DIR/blacklist.txt" -c "kill $$" &
-
-while read -r XPROPOUTPUT; do
-        XPROP_TRACKDATA="$(echo "$XPROPOUTPUT" | cut -d \" -f 2 )"
-
-        # show something
-        # echo "Checking against: $XPROP_TRACKDATA"
-
-        #amixer -D pulse set Master unmute
-        unmute
-        while read -r LINE; do
-            #echo Checking $LINE
-            if grep -Fq "$LINE" <(echo "$XPROP_TRACKDATA"); then
-                #amixer -D pulse set Master mute
-                mute
-                break
-            fi
-        done < "$DIR/blacklist.txt"
-done < <(xprop -spy -id "$ID" _NET_WM_NAME)
